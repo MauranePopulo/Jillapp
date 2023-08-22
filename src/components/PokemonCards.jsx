2d6a79b78efb095c23ecc87b84016d54a06df052
Maurane Populo <maurane.populo@orange.fr>
2023-08-22 16:45:48 +0200

    Quête React 03, affichage pokemon, solution avec operation ternaire

--- src/components/PokemonCards.jsx
+++ src/components/PokemonCards.jsx
@@ -1,8 +1,21 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCards() {
+  const pokemon = pokemonList[0]
+  console.log(pokemon.imgSrc)
     return (
       <div>
-        <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Elephant at sunset" />
-            <figcaption>bulbasaur</figcaption>
+        <figure> {pokemon.imgSrc === undefined ? "???" : <img src= {pokemon.imgSrc} alt={pokemon.name}/>}
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
       </div>
     );
